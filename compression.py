d78a24ac6d97b50d483d7857431534fe5d1073de
malcom.jones <malcom.jones@cnu.edu>
2017-05-12 14:31:00 -0400

    updating old scripts with comments

--- compression.py
+++ compression.py
@@ -1,19 +1,35 @@
-def Compression(s):
-    count = 1;
-    ns = "";
+#Compresses a string and outputs the number of repeated characters in a sequence
+
+#Add the ability to read in command line arguments
+import sys
+
+
+#Function definitions
+def compression(s):
+    count = 1
+    ns = ""
     for i in range(0, len(s)):
         if i == (len(s)-1):
-            ns = ns + s[i] + str(count);
-            count = 1;
-            break;
+            ns = ns + s[i] + str(count)
+            count = 1
+            break
         if s[i] == s[i+1]:
-            count += 1;
+            count += 1
         else:
-            ns = ns + s[i] + str(count);
-            count = 1;
+            ns = ns + s[i] + str(count)
+            count = 1
     if len(s) > len(ns):
-        return ns;
+        return ns
     else:
-        return s;
+        return s
 
-print Compression("aaaaaaaaaaagg");
+#Function calls
+if len(sys.argv) == 1: #No aragument was passed
+    w = raw_input("Enter a string to compress: ")
+    print(compression(w))
+elif len(sys.argv) > 2: #Too many arguments were passed
+    print("You entered too many arguments.")
+    v = raw_input("Enter a string to compress: ")
+    print(compression(v))
+else:
+    print(compression(sys.argv[1]))
--- deleteCommentLine.py
+++ deleteCommentLine.py
@@ -1,27 +1,28 @@
+#Given a path and word,
+
 import os
 import glob
 
-path = 'H:\\Python Code\\3D_Canal\\'
+path = './test/'
 list1 = os.listdir(path)
 
-wordToDelete=raw_input('Enter Word to What You Want to Delete:')
+wordToDelete=raw_input("Enter a word you want to delete: ")
 
 sizeWord=len(wordToDelete)
 for infile in list1:
-	print "current file is: " +infile
+	print("current file is: " +infile)
 	inputfile =file(path + infile,'r')
 	(PATH, FILENAME) = os.path.split(infile)
 	(Name, Extension) = os.path.splitext(FILENAME)
-	outFName = 'H:\\Python Code\\New\\'+Name+'output'+'.txt'
+	outFName = path+Name+'output'+'.txt'
 	outputFile=open(outFName,'w')
 	for line in inputfile:
 		firstWord=line[0:sizeWord]
 		if firstWord == wordToDelete:
-			print("This was deleted" + line)
+			print("This was deleted: " + line)
 			outputFile.write(line[2:len(line)])
 		else:
 			outputFile.write(line)
 inputfile.close()
 outputFile.close()
-raw_input()
-
+#raw_input()
